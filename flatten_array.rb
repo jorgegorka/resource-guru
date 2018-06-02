e3b3231ad89f202ad368473e25ea5f370d5d4b75
Jorge Alvarez <jorge@alvareznavarro.es>
2018-06-02 14:11:39 +0200

    Change to module

--- flatten_array.rb
+++ flatten_array.rb
@@ -1,7 +1,7 @@
 # Custom array flatten method
 #
 # This code has been heavily influenced by: http://blog.prettymuchallofthetime.com/algorithms/2015/10/31/flattening-arrays.html
-class ArrayUtils
+module ArrayUtils
   def self.flatten(array)
     array.reduce([]) do |result, item|
       if item.is_a? Array
